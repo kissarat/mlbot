047ab39ec0e532d180dea88a2cc8b41e240e40f1
Taras Labiak <kissarat@gmail.com>
2016-12-22 12:26:13 +0200

    Utopia

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const {app, Tray, Menu, BrowserWindow} = require('electron')
 const path = require('path')
+const Skype = require('skyweb')
+const skype = new Skype()
+skype.login('kissarat', 'tX*82yU#1+')
+  .then(function (account) {
+    return skype.contactsService.loadContacts(account, function (a, b, c) {
+      console.log(b, c)
+    })
+  })
+  .catch(function (err) {
+    console.error(err)
+  })
 
 BrowserWindow.prototype.loadFile = function (path) {
   return this.loadURL(`file://${__dirname}${path}`)
@@ -12,5 +23,10 @@ let win = null
 app.on('ready', function () {
   win = new BrowserWindow({})
   win.loadFile('/skype.html')
+
+  win.webContents.on('did-finish-load', function() {
+    console.log(win.webContents.getURL());
+  });
+
   win.openDevTools()
 })
--- waiter.js
+++ waiter.js
@@ -170,25 +170,57 @@ window.keypress = Element.prototype.keypress = function keypress(key, code) {
   this.dispatchEvent(new KeyboardEvent('keypress', {key, code: key, charCode: code, keyCode: code}))
 }
 
-Element.prototype.click = function keypress() {
-  this.dispatchEvent(new MouseEvent())
+Element.prototype.click = function keypress(key, code) {
+  this.dispatchEvent(new MouseEvent('click'))
 }
 
 if (this.modules && modules.exports) {
   modules.exports = waiter
 }
 
-waiter('[role=search]', function (input) {
-  input.focus()
-  input.value = 'kissarat'
-  input.keypress('Enter', 13)
-  waiter('.searchDictionary', function (button) {
-    button.click()
-    waiter('.directory li:nth-child(2)', function (li) {
-      li.click()
-      waiter('.contactRequestSend', function (requestButton) {
-        requestButton.click()
+function addFriend(loginName) {
+  waiter('[role=search]', function (input) {
+    input.focus()
+    input.value = loginName
+    input.keypress('Enter', 13)
+    waiter('.searchDictionary', function (button) {
+      button.click()
+      waiter('.directory li:nth-child(2)', function (li) {
+        li.click()
+        waiter('.contactRequestSend', function (requestButton) {
+          requestButton.click()
+        })
       })
     })
   })
-})
+}
+
+function login(loginName, password) {
+  waiter({
+    form: '#i0281',
+    email: '[type=email]'
+  }, function ({form, email}) {
+    email.value = loginName
+    form.submit()
+  })
+  waiter({
+    form: '#i0281',
+    password: '[type=password]',
+    remember: '[name=KMSI]'
+  }, function ({form, password, remember}) {
+    password.value = ''
+    password.value = password
+    remember.checked = true
+    form.submit()
+  })
+}
+
+function logout() {
+  waiter('.Me-sky', function (button) {
+    button.click()
+    waiter('.Me-linkText', function (button) {
+      button.click()
+    })
+  })
+}
+
