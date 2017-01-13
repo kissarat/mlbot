28ff1c1503bbd2ee8fc04a2d451ad25a7ef91bd9
Taras Labiak <kissarat@gmail.com>
2017-01-14 01:50:21 +0200

    + dev mode

--- react/app/index.jsx
+++ react/app/index.jsx
@@ -1,27 +1,47 @@
 import React, {Component} from 'react'
-import {Menu, Segment} from 'semantic-ui-react'
+import {Menu, Segment, Checkbox} from 'semantic-ui-react'
 import {hashHistory} from 'react-router'
+import config from '../../app/config'
+import {each} from 'lodash'
+
+function itemUrl(url) {
+  return {
+    active: location.hash.slice(1).indexOf(url) === 0,
+    onClick() {
+      hashHistory.push(url)
+    }
+  }
+}
 
 export default class App extends Component {
-  render() {
-    const items = [
-      {name: 'Аккаунты', url: '/accounts'},
-      {name: 'Рассылка', url: '/delivery'},
-      {name: 'Приглашения', url: '/invite'},
-    ]
-    items.map(function (item) {
-      const url = item.url
-      delete item.url
-      item.onClick = function () {
-        hashHistory.push(url)
+  developerMode(value) {
+    each(document.querySelectorAll('webview'), function (webview) {
+      if (value) {
+        webview.openDevTools()
       }
-      if (location.hash.slice(1).indexOf(url) === 0) {
-        item.active = true
+      else {
+        webview.closeDevTools()
       }
     })
+    localStorage.setItem('dev', value ? 1 : 0)
+  }
 
+  render() {
+    const devMode = config.dev ?
+      <Menu.Item>
+        <Checkbox
+          label="Режим разработчика"
+          value={+localStorage.getItem('dev') ? 'dev' : ''}
+          onChange={(e, d) => this.developerMode(d.checked)}/>
+      </Menu.Item>
+    : ''
     return <div className="layout app">
-      <Menu attached="top" items={items}/>
+      <Menu attached="top">
+        <Menu.Item name="Аккаунты" {...itemUrl('/accounts')}/>
+        <Menu.Item name="Рассылка" {...itemUrl('/delivery')}/>
+        <Menu.Item name="Приглашения" {...itemUrl('/invite')}/>
+        {devMode}
+      </Menu>
       <Segment attached="bottom" className="content">{this.props.children}</Segment>
     </div>
   }
