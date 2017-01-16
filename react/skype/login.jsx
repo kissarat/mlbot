cf45f565e919f4cae9307f16103921c496c19eeb
Taras Labiak <kissarat@gmail.com>
2017-01-16 21:58:28 +0200

    .page.skype-login + .back

--- react/skype/login.jsx
+++ react/skype/login.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import Skype from './index.jsx'
-import {Button, Form} from 'semantic-ui-react'
+import {Button, Form, Segment, Header, Icon} from 'semantic-ui-react'
 import {hashHistory} from 'react-router'
+import {Link} from 'react-router'
 
 export default class SkypeLogin extends Component {
   state = {loading: false}
@@ -18,11 +19,19 @@ export default class SkypeLogin extends Component {
   }
 
   render() {
-    return <Form onSubmit={this.onSubmit} loading={this.state.loading}>
-      <h1>Вход в Skype</h1>
-      <Form.Field name="login" label="Логин" control="input" type="text"/>
-      <Form.Field name="password" label="Пароль" control="input" type="password"/>
-      <Button type="submit">Вход</Button>
-    </Form>
+    return <Segment className="page skype-login">
+      <div className="top">
+        <Header as="h1">Вход в Skype</Header>
+        <Link className="back" to="/accounts">
+          <Icon name="chevron circle left" size="large"/>
+          Вернуться к списку аккаунтов
+        </Link>
+      </div>
+      <Form onSubmit={this.onSubmit} loading={this.state.loading}>
+        <Form.Field name="login" label="Логин" control="input" type="text"/>
+        <Form.Field name="password" label="Пароль" control="input" type="password"/>
+        <Button type="submit">Вход</Button>
+      </Form>
+    </Segment>
   }
 }
