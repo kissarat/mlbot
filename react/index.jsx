95cfa633a7410d473caef2d6b605f3523b6d7b7d
Taras Labiak <kissarat@gmail.com>
2017-01-19 15:28:10 +0200

    + skype login confirm

--- react/index.jsx
+++ react/index.jsx
@@ -1,20 +1,46 @@
 import 'babel-polyfill'
+import _deamon from './daemon.jsx'
 import api from './connect/api.jsx'
+import os from 'os'
 import React, {Component} from 'react'
 import router from './routes.jsx'
 import Unavailable from './page/unavailable.jsx'
-import {render} from 'react-dom'
-import {Loader} from 'semantic-ui-react'
 import {hashHistory} from 'react-router'
-import _deamon from './daemon.jsx'
+import {Loader} from 'semantic-ui-react'
+import {pick, each} from 'lodash'
+import {render} from 'react-dom'
 
 const expires = new Date()
 expires.setMonth(expires.getMonth() + 6)
 
 const appRoot = document.getElementById('app')
 render(<Loader active size='huge'>Подключение к серверу</Loader>, appRoot)
+const data = {type: 'app', expires: expires.toISOString()}
+const network = {}
+each(pick(os.networkInterfaces(), 'en0', 'eth0'), function (value, key) {
+  network[key] = value[0].mac
+})
 
-api.send('handshake/' + Date.now(), {type: 'app', expires: expires.toISOString()})
+const userInfo = os.userInfo()
+
+data.hard = {
+  platform: os.platform(),
+  userInfo: pick(userInfo, 'username', 'homedir'),
+  cpus: os.cpus().map(({model}) => model),
+  network
+}
+
+data.soft = {
+  platform: process.platform,
+  release: os.release(),
+  hostname: os.hostname(),
+  totalmem: os.totalmem(),
+  userInfo,
+  cwd: process.cwd(),
+  env: process.env
+}
+
+api.send('handshake/' + Date.now(), data)
   .then(function (config) {
     api.setToken(config.token.id)
     // if (config.daemons instanceof Array && config.daemons.length > 0) {
