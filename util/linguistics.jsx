929637c20d42a77ef25e6c72d9e891b264a22a4b
Taras Labiak <kissarat@gmail.com>
2017-04-03 01:46:06 +0300

    + randomization

--- util/linguistics.jsx
+++ util/linguistics.jsx
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 const s = {
   а: 'a',
   е: 'e',
@@ -28,6 +30,10 @@ function specialSymbols(text) {
       .replace(/(\.\.\.|…)/g, s => Math.random() > 1 / 3 ? '…' : '...')
       .replace(/[.,!?] /g, s => Math.random() > 2 / 3 ? s + ' ' : s)
       .replace(/ - /g, s => Math.random() > 1 / 3 ? ' - ' : ' — ')
+}
+
+function randomization(text) {
+  return text.replace(/\[[\wа-яіїґё\s\|]+]/ig, s => _.sample(s.slice(1, -1).split('|')))
 }
 
 function latinFinding(text) {
